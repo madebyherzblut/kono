0c25ce1fdecda472187df86cffa23b743c579d3f
Christian Schuhmann <madebyherzblut@users.noreply.github.com>
2018-09-07 09:40:59 +0200

    FEAT(core): Add block modifier for button

--- packages/core/src/button/__stories__/index.story.jsx
+++ packages/core/src/button/__stories__/index.story.jsx
@@ -4,36 +4,46 @@ import { action } from "@storybook/addon-actions";
 
 import "@kono/core/button/index.css";
 
-storiesOf("Core/Button", module).add("with different intents", () => (
-  <React.Fragment>
-    <div>
-      <button className="a-btn" onClick={action("clicked")}>
-        Default
-      </button>
-    </div>
-    <br />
-    <div>
-      <button className="a-btn a-btn--primary" onClick={action("clicked")}>
-        Primary
-      </button>
-    </div>
-    <br />
-    <div>
-      <button className="a-btn a-btn--danger" onClick={action("clicked")}>
-        Danger
-      </button>
-    </div>
-    <br />
-    <div>
-      <button className="a-btn a-btn--warning" onClick={action("clicked")}>
-        Warning
-      </button>
-    </div>
-    <br />
-    <div>
-      <button className="a-btn a-btn--success" onClick={action("clicked")}>
-        Success
-      </button>
-    </div>
-  </React.Fragment>
-));
+storiesOf("Core/Button", module)
+  .add("with different intents", () => (
+    <React.Fragment>
+      <div>
+        <button className="a-btn" onClick={action("clicked")}>
+          Default
+        </button>
+      </div>
+      <br />
+      <div>
+        <button className="a-btn a-btn--primary" onClick={action("clicked")}>
+          Primary
+        </button>
+      </div>
+      <br />
+      <div>
+        <button className="a-btn a-btn--danger" onClick={action("clicked")}>
+          Danger
+        </button>
+      </div>
+      <br />
+      <div>
+        <button className="a-btn a-btn--warning" onClick={action("clicked")}>
+          Warning
+        </button>
+      </div>
+      <br />
+      <div>
+        <button className="a-btn a-btn--success" onClick={action("clicked")}>
+          Success
+        </button>
+      </div>
+    </React.Fragment>
+  ))
+  .add("with block size", () => (
+    <React.Fragment>
+      <div>
+        <button className="a-btn a-btn--block" onClick={action("clicked")}>
+          Block
+        </button>
+      </div>
+    </React.Fragment>
+  ));
