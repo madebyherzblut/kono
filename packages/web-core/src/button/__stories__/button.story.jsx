05b4a2a8b7dad33c0b38b8d5b40798d4bf186718
Christian Schuhmann <madebyherzblut@users.noreply.github.com>
2019-12-27 14:50:51 +0100

    feat(web-core): Add basic button example

--- packages/web-core/src/button/__stories__/button.story.jsx
+++ packages/web-core/src/button/__stories__/button.story.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import { action } from "@storybook/addon-actions";
+import * as React from "react";
+import "../../reset/index.scss";
+import "../_index.scss";
 
-export default { title: "Web Core|Button" };
+export default {
+  title: "Button"
+};
 
-export const withText = () => <button>Hello button</button>;
+export const Variants = () => (
+  <>
+    <div>
+      <button className="btn" onClick={action("clicked")}>
+        A default button
+      </button>
+    </div>
+    <div>
+      <button className="btn" disabled onClick={action("clicked")}>
+        A disabled default button
+      </button>
+    </div>
+  </>
+);
 
-export const withEmoji = () => (
-  <button>
-    <span role="img" aria-label="so cool">
-      😀 😎 👍 💯
-    </span>
-  </button>
+export const Links = () => (
+  <a href="#" className="btn">
+    A link button
+  </a>
 );
